962227ff14ddc931509cf31b9dc384ff656583a1
NomiChirps <nomi@goodkitty.club>
2022-08-16 19:56:23 -0700

    clean up and document findings

--- README.md
+++ README.md
@@ -1,34 +1,22 @@
 # missed_interrupt
 
-This is a minimal(?) reproduction of a weird issue I'm having with the Raspberry Pi Pico.
+See https://forums.raspberrypi.com/viewtopic.php?t=338900 for context and discussion.
 
-When using DMA to drive the SPI peripheral in full duplex, it appears to occasionally miss DMA-finished interrupts for the receiving channel. In my actual project, this happens reliably at a wide range of SPI frequencies; here, I've only been able to reproduce it at around 1MHz. It's clearly some kind of timing issue, right? Something between the DREQs and SPI FIFO? Or am I doing something wrong?
+This is a cleaned-up reproduction of the issue:
 
-## pico forums post
+- start two DMA channels simultaneously
+- receive both of their interrupt flags in the same call to the handler
+- attempting to use hw_set_bits or hw_clear_bits to clear INTS1 incorrectly clears *both* flags
 
-Hello! I've been working on a fancy asynchronous DMA-driven SPI C++ library, but I'm having some trouble with the DMA-generated interrupts that are supposed to come at the end of every transfer block. It seems like the interrupt for the receiving channel sometimes completely fails to arrive.
+![scope trace](/DS1Z_QuickPrint4.png)
 
-I've put together a small reproduction of the issue here: https://github.com/NomiChirps/missed_interrupt/blob/main/missed_interrupt/missed_interrupt.cc
+Events from top to bottom and left to right:
+- Channel 4 (blue) toggled in the main loop just before starting the transfer.
+- Channel 3 (cyan) toggled in the interrupt handler about 1us later, after the busy-wait, indicating that DMA channel 1's interrupt bit is set.
+- Channel 2 (pink) toggled in the same interrupt handler, indicating that DMA channel 2's interrupt bit is also set.
+- Channel 1 (yellow) goes high, indicating that some bit in INTS is set.
+- Interrupt handler calls hw_set_bits(INTS, 0)
+- Channel 1 goes low, showing that INTS is now 0.
 
-The setup is to claim 2 DMA channels; one reads from a dummy buffer and writes to the SPI data register (filling the TX FIFO), the other reads from the SPI data register and writes to a different dummy buffer (emptying the RX FIFO). Both are configured to use the appropriate SPI DREQ, and to generate an interrupt at the end of each transfer. I set an exclusive interrupt handler for DMA_IRQ_1 which simply tallies up the number of times each channel completes a transfer, putting the difference in a global variable. Finally, the main loop polls all 3 peripherals (DMA channel 0, DMA channel 1, and SPI) until they all report they're not busy, then triggers both DMA channels simultaneously.
+Precisely the same behavior occurs with hw_clear_bits and hw_xor_bits.
 
-In the long term the difference between the RX DMA interrupt count and the TX DMA interrupt count should stay at zero, because the SPI peripheral reads exactly one byte and puts it into the RX FIFO for every one byte written to the TX FIFO for transmission. Instead, I observe that the imbalance slowly grows over time, at a rate of about 10 per million transfers.
-
-The timing is apparently pretty finicky; with this trimmed-down example code, I've only been able to reproduce the issue at SPI frequency 1MHz and transfer size of 1 byte. The way the interrupt handler checks for which channel's IRQ to acknowledge also seems to be important; if it checks both before acknowledging either, the imbalance grows much more slowly than if it does check0-ack0-check1-ack1. I don't know if that actually has something to do with the problem, or if it's just because this slightly changes the timing.
-
-I'm pretty stumped at this point. Am I doing something subtly wrong in that code? I've configured everything correctly as far as I can tell. This kind of usage should be reliable, right?
-
-## caught on the scope
-
-![scope trace](/DS1Z_QuickPrint1.png)
-
-From top to bottom:
-- Channel 4 (blue) toggles in the main loop just before starting each transfer.
-- Channel 3 (cyan) toggles in the interrupt handler each time the TX channel finishes.
-- Channel 2 (pink) toggles in the interrupt handler each time the RX channel finishes.
-- Channel 1 is the SPI SCLK signal.
-
-Each transfer-start is followed by TX finishing after about 1us, then by RX finishing after about 10us.
-
-The low-to-high transition of channel 4  just before the big gap in the middle is the problematic transfer being started.
-Evidently the TX interrupt takes longer to trigger at that moment. The RX interrupt does not get triggered at all before the main loop polling sees that both channels are idle and starts the next transfer.
--- missed_interrupt/missed_interrupt.cc
+++ missed_interrupt/missed_interrupt.cc
@@ -1,120 +1,60 @@
-#include <cstdio>
-#include <cstring>
-
 #include "hardware/dma.h"
 #include "hardware/gpio.h"
 #include "hardware/irq.h"
-#include "hardware/spi.h"
 #include "pico/time.h"
-#include "pico/sync.h"
-#include "pico/stdio.h"
 
-#if LIB_PICO_STDIO_UART
-#error no
-#endif
-#if LIB_PICO_STDIO_USB
-#error no
-#endif
-#if LIB_PICO_STDIO_SEMIHOSTING
-#error no
-#endif
+int c1_channel;
+int c2_channel;
 
+const int irq_index = 0;
 
-int tx_channel;
-int rx_channel;
-
-const int spi_frequency = 4'000'000;
-
-const int irq_index = 1;
-
-const int debug_gpio_missed = 5;
-const int debug_gpio_tx = 6;
-const int debug_gpio_rx = 7;
+const int debug_gpio_c1 = 6;
+const int debug_gpio_c2 = 7;
 const int debug_gpio_start = 8;
-const int debug_gpio_irq = 9;
-
-volatile bool got_tx = 0;
-volatile bool got_rx = 0;
-
+const int debug_gpio_ints = 9;
 
 void __not_in_flash_func(DMA_ISR)() {
-  // 7: ok
-  // 9: not ok
-  //gpio_put(debug_gpio_irq, !gpio_get(debug_gpio_irq));
-  //gpio_put(debug_gpio_irq, !gpio_get(debug_gpio_irq));
-  busy_wait_us(10);
-  if (dma_hw->ints1 & (1<<rx_channel)) {
-    gpio_put(debug_gpio_rx, !gpio_get(debug_gpio_rx));
-  }
-  if (dma_hw->ints1 & (1<<tx_channel)) {
-    gpio_put(debug_gpio_tx, !gpio_get(debug_gpio_tx));
-  }
-  gpio_put(debug_gpio_irq, dma_hw->ints1);
-  hw_set_bits(&dma_hw->ints1, 0);
-  gpio_put(debug_gpio_irq, dma_hw->ints1);
-  return;
+  // Wait long enough to ensure *both* channels have raised their interrupt.
+  busy_wait_us(1);
 
-  if (dma_hw->ints1 & (1<<rx_channel)) {
-    gpio_put(debug_gpio_rx, !gpio_get(debug_gpio_rx));
-    // Direct assignment works correctly; hw_*_bits doesn't
-    //dma_irqn_acknowledge_channel(irq_index, rx_channel);
-    //hw_clear_bits(&dma_hw->ints1, 1u << rx_channel);
-    //dma_hw->ints1 = 1u << rx_channel;
-    hw_set_bits(&dma_hw->ints1, 0);
-    gpio_put(debug_gpio_irq, dma_hw->ints1);
-    got_rx = 1;
+  volatile uint32_t* ints = irq_index ? &dma_hw->ints1 : &dma_hw->ints0;
+  if (*ints & (1<<c1_channel)) {
+    gpio_put(debug_gpio_c1, !gpio_get(debug_gpio_c1));
   }
-  if (dma_hw->ints1 & (1<<tx_channel)) {
-    gpio_put(debug_gpio_tx, !gpio_get(debug_gpio_tx));
-    // Direct assignment works correctly; hw_*_bits doesn't
-    //dma_irqn_acknowledge_channel(irq_index, tx_channel);
-    //hw_clear_bits(&dma_hw->ints1, 1u << tx_channel);
-    //dma_hw->ints1 = 1u<<tx_channel;
-    hw_set_bits(&dma_hw->ints1, 0);
-    gpio_put(debug_gpio_irq, dma_hw->ints1);
-    got_tx = 1;
+  if (*ints & (1<<c2_channel)) {
+    gpio_put(debug_gpio_c2, !gpio_get(debug_gpio_c2));
   }
-  //gpio_put(debug_gpio_irq, !gpio_get(debug_gpio_irq));
+
+  gpio_put(debug_gpio_ints, *ints);
+
+  // This evidently clears both channels, as does any other hw_*_bits()
+  hw_clear_bits(ints, 0);
+  // This would clear just the one channel, as advertised. (and put the interrupt handler into a loop because c2_channel is never cleared)
+  //*ints = 1<<c1_channel;
+
+  gpio_put(debug_gpio_ints, *ints);
 }
 
 int main() {
-  busy_wait_ms(100);
+  busy_wait_ms(1000);
 
-  gpio_init(debug_gpio_missed);
-  gpio_init(debug_gpio_tx);
-  gpio_init(debug_gpio_rx);
+  gpio_init(debug_gpio_c1);
+  gpio_init(debug_gpio_c2);
   gpio_init(debug_gpio_start);
-  gpio_init(debug_gpio_irq);
-  gpio_set_dir(debug_gpio_missed, GPIO_OUT);
-  gpio_set_dir(debug_gpio_tx, GPIO_OUT);
-  gpio_set_dir(debug_gpio_rx, GPIO_OUT);
+  gpio_init(debug_gpio_ints);
+  gpio_set_dir(debug_gpio_c1, GPIO_OUT);
+  gpio_set_dir(debug_gpio_c2, GPIO_OUT);
   gpio_set_dir(debug_gpio_start, GPIO_OUT);
-  gpio_set_dir(debug_gpio_irq, GPIO_OUT);
+  gpio_set_dir(debug_gpio_ints, GPIO_OUT);
 
-  spi_init(spi1, spi_frequency);
-  spi_set_format(spi1, 8, SPI_CPOL_0, SPI_CPHA_0, SPI_MSB_FIRST);
-  gpio_set_function(10, GPIO_FUNC_SPI);
-  gpio_set_function(11, GPIO_FUNC_SPI);
-  gpio_set_function(12, GPIO_FUNC_SPI);
-  // sclk = 10
-  // mosi = 11
-  // miso = 12
-
-  // spi_frequency / 1'000'000 seems to trigger it reliably
   const int transfer_count = 4;
+  int c1_read;
+  int c1_write;
+  int c2_read;
+  int c2_write;
 
-  uint8_t dummy1[transfer_count];
-  uint8_t dummy2[transfer_count];
-  volatile void* tx_read = &dummy1;
-  volatile void* tx_write = &spi_get_hw(spi1)->dr;
-  volatile void* rx_read = &spi_get_hw(spi1)->dr;
-  volatile void* rx_write = &dummy2;
-
-  memset(dummy1, 0x55, transfer_count);
-  memset(dummy2, 0x55, transfer_count);
-
-  tx_channel = dma_claim_unused_channel(true);
-  rx_channel = dma_claim_unused_channel(true);
+  c1_channel = dma_claim_unused_channel(true);
+  c2_channel = dma_claim_unused_channel(true);
 
   dma_channel_config c = {};
   // Common options
@@ -127,56 +67,41 @@ int main() {
   channel_config_set_transfer_data_size(&c, DMA_SIZE_8);
 
   // TX
-  channel_config_set_read_increment(&c, true);
+  channel_config_set_read_increment(&c, false);
   channel_config_set_write_increment(&c, false);
-  channel_config_set_dreq(&c, spi_get_dreq(spi1, true));
-  channel_config_set_chain_to(&c, tx_channel);  // self-chain disables it
-  dma_channel_set_config(tx_channel, &c, false);
-  dma_channel_set_read_addr(tx_channel, tx_read, false);
-  dma_channel_set_write_addr(tx_channel, tx_write, false);
-  dma_channel_set_trans_count(tx_channel, transfer_count, false);
+  channel_config_set_dreq(&c, DREQ_FORCE);
+  channel_config_set_chain_to(&c, c1_channel);  // self-chain disables it
+  dma_channel_set_config(c1_channel, &c, false);
+  dma_channel_set_read_addr(c1_channel, &c1_read, false);
+  dma_channel_set_write_addr(c1_channel, &c1_write, false);
 
   // RX
   channel_config_set_read_increment(&c, false);
-  channel_config_set_write_increment(&c, true);
-  channel_config_set_dreq(&c, spi_get_dreq(spi1, false));
-  channel_config_set_chain_to(&c, rx_channel);  // self-chain disables it
-  dma_channel_set_config(rx_channel, &c, false);
-  dma_channel_set_read_addr(rx_channel, rx_read, false);
-  dma_channel_set_write_addr(rx_channel, rx_write, false);
-  dma_channel_set_trans_count(rx_channel, transfer_count, false);
+  channel_config_set_write_increment(&c, false);
+  channel_config_set_dreq(&c, DREQ_FORCE);
+  channel_config_set_chain_to(&c, c2_channel);  // self-chain disables it
+  dma_channel_set_config(c2_channel, &c, false);
+  dma_channel_set_read_addr(c2_channel, &c2_read, false);
+  dma_channel_set_write_addr(c2_channel, &c2_write, false);
 
-  dma_irqn_set_channel_enabled(irq_index, tx_channel, true);
-  dma_irqn_set_channel_enabled(irq_index, rx_channel, true);
-
+  // IRQs
+  dma_irqn_set_channel_enabled(irq_index, c1_channel, true);
+  dma_irqn_set_channel_enabled(irq_index, c2_channel, true);
   irq_set_exclusive_handler(irq_index ? DMA_IRQ_1 : DMA_IRQ_0, &DMA_ISR);
-  irq_set_priority(irq_index ? DMA_IRQ_1 : DMA_IRQ_0, 128);
   irq_set_enabled(irq_index ? DMA_IRQ_1 : DMA_IRQ_0, true);
 
-  uint32_t num_transfers = 0;
   for (;;) {
+    dma_channel_set_trans_count(c1_channel, transfer_count, false);
+    dma_channel_set_trans_count(c2_channel, transfer_count, false);
+
     // start next cycle
-    ++num_transfers;
-    got_tx = 0;
-    got_rx = 0;
-    dma_channel_set_read_addr(tx_channel, tx_read, false);
-    dma_channel_set_write_addr(rx_channel, rx_write, false);
-    dma_channel_set_trans_count(tx_channel, transfer_count, false);
-    dma_channel_set_trans_count(rx_channel, transfer_count, false);
     gpio_put(debug_gpio_start, !gpio_get(debug_gpio_start));
-    dma_start_channel_mask((1u << tx_channel) | (1u << rx_channel));
+    dma_start_channel_mask((1u << c1_channel) | (1u << c2_channel));
 
     // wait for it to finish
-    const uint32_t max_wait = 500;
-    uint32_t wait_time = 0;
-    //while (!got_tx || !got_rx || dma_irqn_get_channel_status(irq_index, tx_channel) || dma_irqn_get_channel_status(irq_index, rx_channel) || dma_hw->intr || dma_channel_is_busy(tx_channel) || dma_channel_is_busy(rx_channel) || spi_is_busy(spi1)) {
-    while (!got_tx || !got_rx || dma_irqn_get_channel_status(irq_index, tx_channel) || dma_irqn_get_channel_status(irq_index, rx_channel) || dma_channel_is_busy(tx_channel) || dma_channel_is_busy(rx_channel) || spi_is_busy(spi1)) {
-        if(wait_time++ > max_wait) break;
-    }
-    if (wait_time > max_wait) {
-        gpio_put(debug_gpio_missed, !gpio_get(debug_gpio_missed));
+    while (dma_irqn_get_channel_status(irq_index, c1_channel) || dma_irqn_get_channel_status(irq_index, c2_channel) || dma_channel_is_busy(c1_channel) || dma_channel_is_busy(c2_channel)) {
+      // busy-wait for both channels to finish
     }
   }
-
   return 0;
 }
